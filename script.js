e32210fe8daf9334fafa880c0f728ec1befc0bf5
MukundChourasia <144132096+MukundChourasia@users.noreply.github.com>
2024-07-19 10:40:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 var tablinks = document.getElementsByClassName("tab-links");
-    var tabcontents = document.getElementsByClassName("tab-contents");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('JavaScript is working!');
+function opentab(tabname) {
+  for (let tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (let tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxXUHb8Fi5t6V84QdXWmfwnPnwpupbzdaBPxLlN5nnkbIVKWXhghW1C4IcNqjBkt-kSMQ/exec';
+
+fetch(proxyUrl + scriptURL, {
+  headers: {
+    'x-requested-with': 'XMLHttpRequest'
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
+
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(proxyUrl + scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message sent successfully";
+      setTimeout(function () {
+        msg.innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
 });
-
-    function opentab(tabname) {
-      for (tablink of tablinks) {
-        tablink.classList.remove("active-link");
-      }
-      for (tabcontent of tabcontents) {
-        tabcontent.classList.remove("active-tab");
-      }
-      event.currentTarget.classList.add("active-link");
-      document.getElementById(tabname).classList.add("active-tab");
-    }
-    var sidemenu = document.getElementById("sidemenu");
-
-    function openmenu() {
-      sidemenu.style.right = "0";
-    }
-
-    function closemenu() {
-      sidemenu.style.right = "-200px";
-    }
-
-const proxyUrl = 'https://my-updated-portfolio-delta.vercel.app//';
-const targetUrl = 'https://script.google.com/macros/s/AKfycbxXUHb8Fi5t6V84QdXWmfwnPnwpupbzdaBPxLlN5nnkbIVKWXhghW1C4IcNqjBkt-kSMQ/exec'; // Replace this with API URL
-
-fetch(proxyUrl + targetUrl)
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(error => console.error('Error:', error));
-    const scriptURL =
-      "https://script.google.com/macros/s/AKfycbxXUHb8Fi5t6V84QdXWmfwnPnwpupbzdaBPxLlN5nnkbIVKWXhghW1C4IcNqjBkt-kSMQ/exec";
-    const form = document.forms["submit-to-google-sheet"];
-    const msg = document.getElementById("msg");
-
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      fetch(scriptURL, { method: "POST", body: new FormData(form) })
-        .then((response) => {
-          msg.innerHTML = "Message sent succesfully";
-          setTimeout(function () {
-            msg.innerHTML = "";
-          }, 5000);
-          form.reset();
-        })
-        .catch((error) => console.error("Error!", error.message));
-    });
